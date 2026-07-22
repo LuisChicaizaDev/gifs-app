949bd809a42251c659d6bda6f5c9a80e31c7b484
Luis <161586660+luischicaizaDev@users.noreply.github.com>
2026-07-22 17:51:44 +0200

    feat: agregar contenido de ejemplo para dar estilos a la app

--- src/GifsApp.tsx
+++ src/GifsApp.tsx
@@ -1,5 +1,45 @@
+import { mockGifs } from "./mock-data/gifs-mock";
+
 export const GifsApp = () => {
     return (
-        <div>GifsApp</div>
+        <>
+        {/*Header*/}
+        <section className="content-center">
+            <h1>Buscador de Gifs</h1>
+            <p>Descubre y comparte el gif perfecto</p>
+        </section>
+
+        {/* Search */}
+        <section className="search-container">
+            <input type="text" placeholder="Buscar gif" />
+            <button>Buscar</button>
+        </section>
+
+        {/* Búsquedas previas */}
+        <section className="previous-searches">
+            <h2>Búsquedas previas</h2>
+            <ul className="previous-searches-list">
+                <li>Goku</li>
+                <li>Saitama</li>
+                <li>Elden Ring</li>
+            </ul>
+        </section>
+
+        {/* Gifs */}
+        <section className="gifs-container">
+            {
+                mockGifs.map( (gif) => (
+                    <div key={gif.id} className="gif-card">
+                        <img src={gif.url} alt={gif.title} />
+                        <h3> { gif.title } </h3>
+                        <p>
+                             {gif.width }x{ gif.height } (1.5MB)
+                        </p>
+                    </div>
+                ))
+            }
+        </section>
+
+        </>
     );
 }
