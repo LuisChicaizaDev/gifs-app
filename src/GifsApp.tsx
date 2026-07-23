029743fc30bbfcfddb023af76e6d274a0c0114f1
Luis <161586660+luischicaizaDev@users.noreply.github.com>
2026-07-23 11:21:53 +0200

    refactor(header): crear componente para el header

--- src/GifsApp.tsx
+++ src/GifsApp.tsx
@@ -1,45 +1,42 @@
+import { CustomHeader } from "./components/CustomHeader";
 import { mockGifs } from "./mock-data/gifs-mock";
 
 export const GifsApp = () => {
-    return (
-        <>
-        {/*Header*/}
-        <section className="content-center">
-            <h1>Buscador de Gifs</h1>
-            <p>Descubre y comparte el gif perfecto</p>
-        </section>
+  return (
+    <>
+      <CustomHeader
+        title="Buscador de gifs"
+        description="Descubre y comparte el gif perfecto"
+      />
 
-        {/* Search */}
-        <section className="search-container">
-            <input type="text" placeholder="Buscar gif" />
-            <button>Buscar</button>
-        </section>
+      {/* Search */}
+      <section className="search-container">
+        <input type="text" placeholder="Buscar gif" />
+        <button>Buscar</button>
+      </section>
 
-        {/* Búsquedas previas */}
-        <section className="previous-searches">
-            <h2>Búsquedas previas</h2>
-            <ul className="previous-searches-list">
-                <li>Goku</li>
-                <li>Saitama</li>
-                <li>Elden Ring</li>
-            </ul>
-        </section>
+      {/* Búsquedas previas */}
+      <section className="previous-searches">
+        <h2>Búsquedas previas</h2>
+        <ul className="previous-searches-list">
+          <li>Goku</li>
+          <li>Saitama</li>
+          <li>Elden Ring</li>
+        </ul>
+      </section>
 
-        {/* Gifs */}
-        <section className="gifs-container">
-            {
-                mockGifs.map( (gif) => (
-                    <div key={gif.id} className="gif-card">
-                        <img src={gif.url} alt={gif.title} />
-                        <h3> { gif.title } </h3>
-                        <p>
-                             {gif.width }x{ gif.height } (1.5MB)
-                        </p>
-                    </div>
-                ))
-            }
-        </section>
-
-        </>
-    );
-}
+      {/* Gifs */}
+      <section className="gifs-container">
+        {mockGifs.map((gif) => (
+          <div key={gif.id} className="gif-card">
+            <img src={gif.url} alt={gif.title} />
+            <h3> {gif.title} </h3>
+            <p>
+              {gif.width}x{gif.height} (1.5MB)
+            </p>
+          </div>
+        ))}
+      </section>
+    </>
+  );
+};
---
+++ src/components/CustomHeader.tsx
@@ -0,0 +1,15 @@
+/** Parametrizamos el componente */
+interface Props {
+  title: string;
+  description?: string;
+}
+
+export const CustomHeader = ({ title, description }: Props) => {
+  return (
+    /* Header */
+    <section className="content-center">
+      <h1>{title}</h1>
+      {description && <p>{description}</p>}
+    </section>
+  );
+};
