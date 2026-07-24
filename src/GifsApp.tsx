172df23e434e01f33bf3ddfe8abdccb6227d0e5e
Luis <161586660+luischicaizaDev@users.noreply.github.com>
2026-07-24 17:03:33 +0200

    refactor: optimizar código con un custom hook para obtener los gifs

--- src/GifsApp.tsx
+++ src/GifsApp.tsx
@@ -1,38 +1,14 @@
-import { useState } from 'react';
 import { CustomHeader } from './components/CustomHeader';
 import { SearchBar } from './components/SearchBar';
 import { GifsList } from './gifs/components/GifsList';
 import { PreviousSearches } from './gifs/components/PreviousSearches';
-import { getGifsByQuery } from './gifs/actions/get-gifs-by-query';
-import type { Gif } from './gifs/interfaces/gif-interface';
+
+import { useGifs } from './gifs/hooks/useGifs';
 
 export const GifsApp = () => {
-  // Indicamos el tipo de dato que guardamos, un array de objetos tipo Gif
-  const [gifs, setGifs] = useState<Gif[]>([]);
-  const [previousTerms, setPreviousTerms] = useState<string[]>([]);
-
-  const handlePreviousClicked = (term: string) => {
-    console.log({ term });
-  };
-
-  // Controlamos la consulta del usuario
-  const handleSearch = async (query: string) => {
-    query = query.trim().toLowerCase();
-
-    if (query === '') return;
-
-    // Comprobamos que el termino no existe anteriormente
-    if (previousTerms.includes(query)) return;
-
-    // Agregamos el término al inicio y recortamos el array a 8 elementos
-    setPreviousTerms([query, ...previousTerms].splice(0, 8));
-
-    // Pasamos la query para hacer la petición
-    const gifs = await getGifsByQuery(query);
-
-    // Actualizamos el estado con los gifs que vienen de la petición
-    setGifs(gifs);
-  };
+  // Desestrucutamos el objeto devuelto del custom hook
+  const { gifs, previousTerms, handlePreviousClicked, handleSearch } =
+    useGifs();
 
   return (
     <>
---
+++ src/gifs/hooks/useGifs.tsx
@@ -0,0 +1,43 @@
+import { useState } from 'react';
+import type { Gif } from '../interfaces/gif-interface';
+import { getGifsByQuery } from '../actions/get-gifs-by-query';
+
+// Custom Hook
+export const useGifs = () => {
+  // Indicamos el tipo de dato que guardamos, un array de objetos tipo Gif
+  const [gifs, setGifs] = useState<Gif[]>([]);
+  const [previousTerms, setPreviousTerms] = useState<string[]>([]);
+
+  const handlePreviousClicked = (term: string) => {
+    console.log({ term });
+  };
+
+  // Controlamos la consulta del usuario
+  const handleSearch = async (query: string) => {
+    query = query.trim().toLowerCase();
+
+    if (query === '') return;
+
+    // Comprobamos que el termino no existe anteriormente
+    if (previousTerms.includes(query)) return;
+
+    // Agregamos el término al inicio y recortamos el array a 8 elementos
+    setPreviousTerms([query, ...previousTerms].splice(0, 8));
+
+    // Pasamos la query para hacer la petición
+    const gifs = await getGifsByQuery(query);
+
+    // Actualizamos el estado con los gifs que vienen de la petición
+    setGifs(gifs);
+  };
+
+  return {
+    // Props
+    gifs,
+    previousTerms,
+
+    // Methods
+    handlePreviousClicked,
+    handleSearch,
+  };
+};
