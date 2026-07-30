96d9c7359dd9930ab4918ef9cadf4e59323d0c5f
Luis <161586660+luischicaizaDev@users.noreply.github.com>
2026-07-30 12:36:53 +0200

    chore: agregar configuración inicial para el testing

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,14 @@
-import { defineConfig } from 'vite'
-import react, { reactCompilerPreset } from '@vitejs/plugin-react'
-import babel from '@rolldown/plugin-babel'
+// import { defineConfig } from 'vite'
+import { defineConfig } from 'vitest/config'; // config para los test de componentes
+import react, { reactCompilerPreset } from '@vitejs/plugin-react';
+import babel from '@rolldown/plugin-babel';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    babel({ presets: [reactCompilerPreset()] })
-  ],
-})
+  plugins: [react(), babel({ presets: [reactCompilerPreset()] })],
+  // config para los test de componentes
+  test: {
+    environment: 'jsdom',
+    globals: true,
+  },
+});
